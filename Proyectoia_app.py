d3866e6c1570197e3b18d96b70f9b5882bb10c68
StefanyTP07 <106336762+StefanyTP07@users.noreply.github.com>
2022-06-16 00:39:43 -0500

    Update Proyectoia_app.py

--- Proyectoia_app.py
+++ Proyectoia_app.py
@@ -8,5 +8,18 @@ def download_data():
  filename = 'data.csv'
  urllib.request.urlretrieve(url, filename)
 download_data()
+import matplotlib.pyplot as plt
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+import plotly.figure_factory as ff
+import scipy
+df_bonilla = pd.read_csv(r'https://drive.google.com/uc?id=17B8hm_07RhiLpL0GPhuvEQumycgzowez', header= 0) 
+#df_miraflores= pd.read_csv(r'https://www.datosabiertos.gob.pe/sites/default/files/Monitoreo_setiembre_Ov.Miraflores.xlsx', header= 0) 
 
-
+st.title("Análisis Bonilla")
+st.header("Tabla de datos")
+st.write(df_bonilla)
+for i in range(6,15):
+    fig = px.histogram(df_bonilla, df_bonilla.columns[i])
+    st.plotly_chart(fig, use_container_width=True)
